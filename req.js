600285fc4c7bcb8fe86b44b9706988ad287e94e9
Moises Ribeiro <ribeirojr@gmail.com>
2015-09-30 18:50:53 +0100

    Helper files removed

--- req.js
+++
@@ -1,5 +0,0 @@
-var Wreck = require('wreck');
-
-Wreck.get('http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s=BRLEUR=X', { redirects: 3  }, function(err, res, body) {
-	 console.log(body.toString());		
-});
